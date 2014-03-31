27842db58114b70fe725b90fc1164f9b719e7fbd
Jeff Zhang <fzz727@hotmail.com>
2014-03-31 12:32:57 +0800

    Add GMap.net code.

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -16,7 +16,7 @@ namespace TestApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmGMap());
         }
     }
 }
---
+++ TestApplication/frmGMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApplication
+{
+    public partial class frmGMap : Form
+    {
+        public frmGMap()
+        {
+            InitializeComponent();
+
+            GMap.NET.WindowsForms.GMapControl map = new GMap.NET.WindowsForms.GMapControl();
+            map.Width = 500;
+            map.Height = 400;
+
+            this.Controls.Add(map);
+        }
+    }
+}
